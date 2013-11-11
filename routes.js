77b0f210ffcd26240dd25c14d6b13ce2cb788c2f
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-11-11 20:30:53 +0700

    Standalone map, activity list/view operations

--- routes.js
+++ routes.js
@@ -8,5 +8,7 @@ module.exports = function(app) {
   app.get('/', controllers.home.index);
   app.get('/worldbank/index', controllers.worldbank.index);
   app.get('/api/points.json', controllers.api.points);
-  app.get('/map', controllers.map)
+  app.get('/map', controllers.activity.map);
+  app.get('/activities', controllers.activity.index);
+  app.get('/activity/:id', controllers.activity.view);
 }
--- views/helpers.js
+++ views/helpers.js
@@ -1,3 +1,19 @@
+var markdown = require('marked');
+
 exports.json = function(obj) {
   return JSON.stringify(obj);
+}
+
+exports.markdown = function(text) {
+  return text ? markdown(text.trim()) : '';
+}
+
+map = exports.map = function(lat, lon) {
+  // http://maps.google.com/maps?z=12&t=m&q=loc:38.9419+-78.3020
+  return 'http://maps.google.com/maps?z=12&t=m&q=loc:' + lat + '+' + lon;
+}
+
+code = exports.code = function(collection, code) {
+  console.log(collection);
+  return codelists.getName(collection, code);
 }
