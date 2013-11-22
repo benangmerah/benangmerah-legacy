992a8da4fb84b028805d78fbbdeb1cf78eb2c86d
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-11-22 11:38:20 +0700

    Modified Datasource.js
    Added nextPull and interval properties; the latter powered by moment.

--- models/Datasource.js
+++ models/Datasource.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose'),
+    moment = require('moment'),
     Schema = mongoose.Schema;
 
 var datasourceSchema = new Schema({
@@ -6,6 +7,8 @@ var datasourceSchema = new Schema({
   description: String,  // A description for this datasource
   driver: String,       // The driver used to pull from this datasource. Must be in /drivers
   ref: String,          // A URI or other identifier used by the driver to identify where to pull from
+  nextPull: Date,       // When to do the next pull
+  interval: String,     // Pull interval: [Number] (days|months|years)
   paramsJSON: String    // A JSON-encoded object of parameters to feed the driver, such as API key
 });
 
@@ -16,5 +19,12 @@ datasourceSchema.virtual('params')
   .set(function(v){
     this.paramsJSON = JSON.stringify(v);
   });
+
+datasourceSchema.methods.getIntervalDuration = function() {
+  var interval = this.interval;
+  var split = interval.split(' ');
+  var duration = moment.duration(split[0], split[1]);
+  return duration;
+}
 
 module.exports = mongoose.model('Datasource', datasourceSchema);
