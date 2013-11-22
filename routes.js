abccbf526822ecd55096d901ab7ebcbb7965b09a
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-11-22 15:15:23 +0700

    Added Syahbandar: Datasource management GUI

--- routes.js
+++ routes.js
@@ -11,4 +11,10 @@ module.exports = function(app) {
   app.get('/map', controllers.activity.map);
   app.get('/activities', controllers.activity.index);
   app.get('/activity/:id', controllers.activity.view);
+  app.get('/syahbandar/index', controllers.syahbandar.index);
+  app.get('/syahbandar/details/:id', controllers.syahbandar.details);
+  app.get('/syahbandar/edit/:id', controllers.syahbandar.edit);
+  app.post('/syahbandar/edit/:id', controllers.syahbandar.processEdit);
+  app.post('/syahbandar/pull/:id', controllers.syahbandar.pull);
+  app.get('/syahbandar', controllers.syahbandar.index);
 }
