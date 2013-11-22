fafbac37b36a88c3030c35bd54c7e576e5195062
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-11-22 18:57:35 +0700

    Updated datasource

--- models/Datasource.js
+++ models/Datasource.js
@@ -1,23 +1,26 @@
-var mongoose = require('mongoose'),
+var querystring = require('querystring'),
+    mongoose = require('mongoose'),
     moment = require('moment'),
     Schema = mongoose.Schema;
 
 var datasourceSchema = new Schema({
   _id: String,          // A unique, no-spaces but human-readable identifier
+  title: String,        // Full title of the datasource
   description: String,  // A description for this datasource
   driver: String,       // The driver used to pull from this datasource. Must be in /drivers
   ref: String,          // A URI or other identifier used by the driver to identify where to pull from
+  lastPull: Date,       // When the datasource was last pulled
   nextPull: Date,       // When to do the next pull
   interval: String,     // Pull interval: [Number] (days|months|years)
-  paramsJSON: String    // A JSON-encoded object of parameters to feed the driver, such as API key
+  paramsQS: String      // Parameters to feed the driver (such as API key) in query string form
 });
 
 datasourceSchema.virtual('params')
   .get(function() {
-    return JSON.parse(this.paramsJSON);
+    return this.paramsQS ? querystring.parse(this.paramsQS.trim()) : {};
   })
   .set(function(v){
-    this.paramsJSON = JSON.stringify(v);
+    this.paramsQS = querystring.stringify(v);
   });
 
 datasourceSchema.methods.getIntervalDuration = function() {
@@ -26,5 +29,10 @@ datasourceSchema.methods.getIntervalDuration = function() {
   var duration = moment.duration(split[0], split[1]);
   return duration;
 }
+
+datasourceSchema.virtual('driverTitle').get(function() {
+  var meta = require('../drivers').meta[this.driver];
+  return meta ? meta.title : this.driver;
+});
 
 module.exports = mongoose.model('Datasource', datasourceSchema);
