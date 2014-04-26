59cdc4c3db76a159ff0b924d63371fc5a8f7a7f2
Andhika Nugraha <andhika.nugraha@outlook.com>
2014-04-26 21:41:09 +0100

    Rudimentary list of provinces for home/index
    Just to show the transition to Stardog

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,62 @@
-var Activity = require('../models').Activity;
+var url = require('url');
+var async = require('async');
+var conn = require('../starmutt');
 
-exports.index = function(req, res) {
-  Activity.count({}, function(err, count) {
-    res.render('home/index', {
-      activityCount: count,
-      orgCount: 1,
-      maps: true
+exports.index = function(req, res, next) {
+  function execQuery(callback) {
+    var query = 'select distinct ?uri ?label \
+      where { ?uri a bm:Provinsi. ?uri rdfs:label ?label }';
+
+    conn.getResults(query, callback);
+  }
+
+  function processResults(results, callback) {
+    var provinceLabels = {};
+
+    results.forEach(function(result) {
+      var uri = result.uri.value;
+      var existingLabel = provinceLabels[uri];
+
+      if (/^Prov/.test(result.label.value)) {
+        // This label is too verbose
+        return;
+      }
+
+      if (!existingLabel) {
+        var label = result.label.value;
+        provinceLabels[uri] = result.label;
+        return;
+      }
+
+      var existingLang = existingLabel['xml:lang'];
+      if (existingLang !== 'id' && result.label['xml:lang'] === 'id') {
+        // Override the label
+        provinceLabels[uri] = result.label;
+      }
     });
-  })
+
+    var provinces = [];
+    for (var provinceURI in provinceLabels) {
+      provinces.push({
+        path: url.parse(provinceURI).path, // Remove the domain name
+        name: provinceLabels[provinceURI].value
+      });
+    }
+
+    callback(null, provinces);
+  }
+
+  function render(err, provinces) {
+    // `provinces` is an array of { path: ..., name: ... }
+    if (err) {
+      next(err);
+    }
+    else {
+      res.render('home/index', {
+        provinces: provinces
+      });
+    }
+  }
+
+  async.waterfall([execQuery, processResults], render);
 }
