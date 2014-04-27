e404f8df7db91916113199797e4a275494f77851
Andhika Nugraha <andhika.nugraha@outlook.com>
2014-04-27 03:17:33 +0100

    Added ontology routing

--- routes/ontology.js
+++ routes/ontology.js
@@ -3,6 +3,10 @@ var express = require('express');
 var url = require('url');
 var async = require('async');
 var conn = require('starmutt');
+var cache = require('memory-cache');
+var config = require('config');
+
+var lifetime = config.cachelifetime || 100;
 
 var ontologyDefinition = 'https://raw.githubusercontent.com/benangmerah/wilayah/master/ontology.ttl';
 var redirectPlacesTo = 'https://raw.githubusercontent.com/benangmerah/wilayah/master/instances.ttl';
@@ -17,36 +21,114 @@ function derefOntology(req, res, next) {
 function describeInternalResource(req, res, next) {
   var originalUrl = req.originalUrl;
   req.resourceURI = 'http://benangmerah.net' + originalUrl;
-  console.log(req.resourceURI);
   req.url = req.resourceURI;
   next();
 }
 
-function describeResource(req, res, next) {
-  if (!req.resourceURI) {
+var ontologyRoutes = [];
+function ontologyRouter(req, res, next) {
+  var uri = req.resourceURI;
+
+  if (!uri) {
     next();
   }
 
-  function execQuery(callback) {
-    var query = util.format('select ?type where { <%s> a ?type }', req.resourceURI);
-    conn.getColValues(query, callback);
+  function resolveTypes(callback) {
+    var query = util.format('select ?type where { <%s> a ?type }', uri);
+    conn.getColValues({ query: query, reasoning: 'QL' }, function(err, resolvedTypes) {
+      if (err) {
+        callback(err);
+      }
+      else {
+        cache.put('resolvedTypes:' + uri, resolvedTypes, lifetime);
+        callback(null, resolvedTypes);
+      }
+    });
   }
 
-  function resourceType(type) {
-    return this.indexOf(type) !== -1;
-  }
-
-  function render(err, col) {
+  function callMatchingRoute(err, resolvedTypes) {
     if (err) {
       return next(err);
     }
 
-    res.json(col);
+    var found = false;
+    ontologyRoutes.forEach(function(route, idx) {
+      if (found) {
+        return;
+      }
+
+      var classURI = route.classURI;
+      if (resolvedTypes.indexOf(classURI) !== -1) {
+        console.log('Found: ' + classURI);
+        cache.put('matchedOntologyRouteIndex:' + uri, idx, lifetime);
+        ontologyRoutes[idx].callback(req, res, next);
+        found = true;
+        return;
+      }
+    });
+
+    if (!found) {
+      next();
+    }
   }
 
-  async.waterfall([execQuery], render);
+  var cachedRouteIndex = cache.get('matchedOntologyRouteIndex:' + uri);
+  var cachedResolvedTypes = cache.get('resolvedTypes:' + uri);
+
+  if (cachedRouteIndex) {
+    console.log('Found in cache: ' + uri + ' a ' + ontologyRoutes[cachedRouteIndex].classURI);
+    ontologyRoutes[cachedRouteIndex].callback(req, res, next);
+    return;
+  }
+  else if (cachedResolvedTypes) {
+    return callMatchingRoute(null, cachedResolvedTypes);
+  }
+  else {
+    return resolveTypes(callMatchingRoute);
+  }
+}
+ontologyRouter.route = function addOntologyRoute(classURI, callback) {
+  ontologyRoutes.push({
+    classURI: classURI,
+    callback: callback
+  });
+}
+
+function describeProvinsi(req, res, next) {
+  res.json('Hello provinsi');
+}
+
+function describeKota(req, res, next) {
+  res.json('Hello kota');
+}
+
+function describePlace(req, res, next) {
+  res.json('Hello place');
+}
+
+function sameAsFallback(req, res, next) {
+  if (!req.resourceURI) {
+    next();
+  }
+
+  var query = util.format('select distinct ?twin \
+    where { { ?twin owl:sameAs <%s> } \
+    union { <%s> owl:sameAs ?twin } }', req.resourceURI);
+  conn.getColValues(query, function(err, col) {
+    if (err) {
+      return next(err);
+    }
+    if (col.length === 1) {
+      res.send('Redirectable to ' + col[0]);
+    }
+  });
 }
 
 router.use('/ontology', derefOntology);
 router.use('/place', describeInternalResource);
-router.use(describeResource);
+
+ontologyRouter.route('http://benangmerah.net/ontology/Provinsi', describeProvinsi);
+ontologyRouter.route('http://benangmerah.net/ontology/Place', describePlace);
+ontologyRouter.route('http://benangmerah.net/ontology/Kota', describeKota);
+router.use(ontologyRouter);
+router.use(sameAsFallback);
