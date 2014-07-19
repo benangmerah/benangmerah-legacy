6dd728de9ba99144e10adb6390fe793ede952a48
Andhika Nugraha <andhika.nugraha@outlook.com>
2014-07-19 10:15:59 +0700

    Redis etc
    Redis caching for output & stardog for performance.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var config = require('config');
 var conn = require('starmutt');
+var redis = require('redis');
 
 // express middleware
 var hbs = require('hbs');
@@ -26,6 +27,41 @@ var dataManager = require('./routes/data-manager');
 // CONFIG INIT ---
 if (!config.port) {
   config.port = process.env.PORT || 3000;
+}
+if (!config.outputCache) {
+  config.outputCache = {};
+}
+
+// STARDOG INIT ---
+if (config.stardog) {
+  conn.setEndpoint(config.stardog.endpoint);
+  conn.setCredentials(config.stardog.username, config.stardog.password);
+  conn.setDefaultDatabase(config.stardog.database);
+}
+
+// REDIS INIT ---
+if (config.redis) {
+  try {
+    var redisClient = redis.createClient();
+    config.outputCache.cacheClient = redisClient;
+
+    if (config.stardog && config.stardog.cache) {
+      if (typeof config.cache === 'object') {
+        conn.setCacheClient(redisClient, config.stardog.cache.ttl);
+      }
+      else {
+        conn.setCacheClient(redisClient);
+      }
+    }
+  }
+  catch (e) {
+    console.log('Failed creating redis client.');
+    config.outputCache.skipCache = true;
+    conn.setCacheClient(null);
+  }
+}
+else {
+  config.outputCache.skipCache = true;
 }
 
 // EXPRESS INIT ---
@@ -50,6 +86,7 @@ app.set('view options', { layout: 'layouts/main' });
 // express: middleware before routing
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(serveStatic(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride());
 app.use('/css', lessMiddleware(
@@ -59,33 +96,36 @@ app.use('/css', lessMiddleware(
     compiler: {
       compress: ('development' !== app.get('env')) } }
 ));
-app.use(serveStatic(__dirname + '/public'));
 
-// app router
+// Data Manager - do not cache
 app.use('/data-manager', dataManager);
+
+// Everything else - cache
 app.use(outputCache(config.outputCache));
 app.use(routes);
 app.use(ontologyRoutes);
 
-// debugging
-outputCache.on('hit', function(key, req) {
-  console.log('cache HIT: ' + req.originalUrl);
-});
-outputCache.on('miss', function(key, req) {
-  console.log('cache MISS: ' + req.originalUrl);
-});
-outputCache.on('save', function(key, obj) {
-  console.log('cache SAVE: ' + obj.body.length);
-});
-if ('development' == app.get('env')) {
+// DEBUGGING ---
+
+if ('development' === app.get('env')) {
   app.use(errorHandler({ dumpExceptions: true, showStack: true }));
 }
-
-// STARDOG INIT ---
-if (config.stardog) {
-  conn.setEndpoint(config.stardog.endpoint);
-  conn.setCredentials(config.stardog.username, config.stardog.password);
-  conn.setDefaultDatabase(config.stardog.database);
+if (config.redis) {
+  function onHit(key) {
+    console.log('Cache HIT  ' + key);
+  }
+  function onMiss(key) {
+    console.log('Cache MISS ' + key);
+  }
+  function onPut(key) {
+    console.log('Cache PUT  ' + key);
+  }
+  conn.cacheEvents.on('hit', onHit);
+  conn.cacheEvents.on('miss', onMiss);
+  conn.cacheEvents.on('put', onPut);
+  outputCache.on('hit', onHit);
+  outputCache.on('miss', onMiss);
+  outputCache.on('put', onPut);
 }
 
 // RUN EXPRESS ---
